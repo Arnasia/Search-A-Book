7a4beca27e557879961b781f512afeb8d32cfe14
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-04-07 18:32:03 -0400

    remove book

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -40,6 +40,7 @@ const resolvers = {
         const token = signToken(user);
         return { token, user };
       },
+      
       saveBook: async (parent, { input }, context) => {
         if (context.user) {
           const updatedUser = await User.findByIdAndUpdate(
@@ -51,6 +52,17 @@ const resolvers = {
         }
         throw new AuthenticationError('You need to be logged in!')
       },
-  
+
+      removeBook: async (parent, args, context) => {
+        if (context.user) {
+          const updatedUser = await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $pull: { savedBooks: { bookId: args.bookId } } },
+            { new: true }
+          );
+          return updatedUser;
+        }
+        throw new AuthenticationError('You need to be logged in!')
+      } 
     }
 }
