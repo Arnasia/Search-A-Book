20e0cb1744d0f6475ce7b1953701765d9a9e49e2
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-04-07 19:07:11 -0400

    mutation to add user

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -22,3 +22,26 @@ export const LOGIN_USER = gql`
      }
    }
 `;
+
+// add user 
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+        token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    bookId
+                    title
+                    description
+                    authors
+                    image
+                    link
+                }
+            }
+        }
+    }
+`;
