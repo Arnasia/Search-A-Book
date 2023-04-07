0a180db9d557799c315b89dc517e69c51eb7eb93
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-04-07 18:39:46 -0400

    mutation type

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -36,4 +36,11 @@ const typeDefs = gql`
     me: User
   }
 
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: bookInput): User
+    removeBook(bookId: String!): User
+  }
+
   `;
