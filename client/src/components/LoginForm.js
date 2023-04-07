52d595a74c171edf5e5f26a25126437feaacd33c
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-04-07 19:24:35 -0400

    fixing bug

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -26,25 +26,13 @@ const LoginForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await loginUser({
+        variables: {...userFormData} 
+      });
+      Auth.login(data.login.token);
     } catch (err) {
-      console.error(err);
+      console.error(e);
       setShowAlert(true);
     }
 
