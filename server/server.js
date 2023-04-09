ca1b53169bb379e235a418d3b700ec7a054855bd
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-04-09 14:22:14 -0400

    fixed start server issue

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
@@ -10,15 +9,14 @@ const { authMiddleware } = require('./utils/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// for apollo server use context ... typeDefs, resolvers, and auth
+// setup apollo server and use typeDefs, resolvers, and auth for context
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
 });
 
-// apollo server with express app as middleware
-server.applyMiddleware({ app });
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -28,15 +26,24 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
+// app.use(routes);
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 })
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-  console.log(`🌍 Now listening on localhost:${PORT}`);
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+const startServer = async (typeDefs, resolvers) => {
+  await server.start()
+// integrate apollo server with express app as middleware
+server.applyMiddleware({ app });
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
   });
-});
+  
+}
+
+startServer(typeDefs, resolvers)
+
+
